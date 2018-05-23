dbb5f059df7b02119608eeafb66379129329eff3
KireinaHoro <i@jsteward.moe>
2018-05-24 01:38:53 +0800

    remove UIDGID_STRICT_TYPE_CHECKS

--- include/linux/uidgid.h
+++ include/linux/uidgid.h
@@ -17,32 +17,6 @@
 struct user_namespace;
 extern struct user_namespace init_user_ns;
 
-#ifdef CONFIG_UIDGID_STRICT_TYPE_CHECKS
-
-typedef struct {
-	uid_t val;
-} kuid_t;
-
-
-typedef struct {
-	gid_t val;
-} kgid_t;
-
-#define KUIDT_INIT(value) (kuid_t){ value }
-#define KGIDT_INIT(value) (kgid_t){ value }
-
-static inline uid_t __kuid_val(kuid_t uid)
-{
-	return uid.val;
-}
-
-static inline gid_t __kgid_val(kgid_t gid)
-{
-	return gid.val;
-}
-
-#else
-
 typedef uid_t kuid_t;
 typedef gid_t kgid_t;
 
@@ -58,8 +32,6 @@ static inline gid_t __kgid_val(kgid_t gid)
 
 #define KUIDT_INIT(value) ((kuid_t) value )
 #define KGIDT_INIT(value) ((kgid_t) value )
-
-#endif
 
 #define GLOBAL_ROOT_UID KUIDT_INIT(0)
 #define GLOBAL_ROOT_GID KGIDT_INIT(0)
