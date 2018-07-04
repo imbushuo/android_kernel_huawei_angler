be819350157b2aadcbc8db7001119130f0e51bad
KireinaHoro <i@jsteward.moe>
2018-07-04 10:56:01 +0800

    enable /bin/installkernel to install the kernel

--- arch/arm64/boot/install.sh
+++ arch/arm64/boot/install.sh
@@ -22,6 +22,7 @@
 
 # User may have a custom install script
 if [ -x ~/bin/${INSTALLKERNEL} ]; then exec ~/bin/${INSTALLKERNEL} "$@"; fi
+if [ -x /bin/${INSTALLKERNEL} ]; then exec /bin/${INSTALLKERNEL} "$@"; fi
 if [ -x /sbin/${INSTALLKERNEL} ]; then exec /sbin/${INSTALLKERNEL} "$@"; fi
 
 if [ "$(basename $2)" = "Image.gz" ]; then
